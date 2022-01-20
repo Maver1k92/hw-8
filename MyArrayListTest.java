e93b5ad5ada9542d4f77e0b6ef88d25aaf600898
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-01-21 00:38:39 +0200

    Update MyArrayListTest.java

--- MyArrayListTest.java
+++ MyArrayListTest.java
@@ -1,20 +1,18 @@
-class MyArrayListTests {
+public class MyArrayListTest {
     public static void main(String[] args) {
-        MyArrayList<String> list = new MyArrayList();
-        list.add("MotionProtect");
-        list.add("DoorProtect");
-        list.add("HomeSiren");
-        list.add("LeaksProtect");
-        list.add("KeyPad");
-        list.add("ReX");
-        list.add("MotionCam");
+        MyArrayList<String> strings = new MyArrayList<>();
+        strings.add("HomeSiren");
+        strings.add("StreetSiren");
+        strings.add("DoorProtect");
+        strings.add("MotionProtect");
+        strings.remove(1);
+        strings.clear();
 
 
-        System.out.println("list = " + list );
-        System.out.println("list.size() = " + list.size());
-        System.out.println("list.get(0) = " + list.get(0));
-        System.out.println(list.remove(2));
-        System.out.println(list.clear());
+        System.out.println(strings.get(0));
+        System.out.println(strings.size());
+
+
     }
 
 }
