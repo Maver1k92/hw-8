4837c5736318fec4977cb2cc1c05a7afd065314b
Maver1k92 <70327932+Maver1k92@users.noreply.github.com>
2022-01-25 23:49:22 +0200

    Update HashMapMethods.java

--- HashMapMethods.java
+++ HashMapMethods.java
@@ -1,7 +1,7 @@
 public interface HashMapMethods<K, V> extends Iterable<V> {
-    boolean put(K key , V value);// добавляю пару ключ + значение
-    void remove(K key);//удаляю пару по ключу
-    void clear();//очищаю коллекцию
-    int size();// размер коллекции
-    V get(K key);// возвращаю значение (value)  по ключу key
+   boolean put(K key , V value);// добавляю пару ключ + значение
+   boolean remove(K key);//удаляю пару по ключу
+   void clear();//очищаю коллекцию
+   int size();// размер коллекции
+   V get(K key);// возвращаю значение (value)  по ключу key
 }
